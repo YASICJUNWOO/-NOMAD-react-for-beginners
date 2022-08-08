6a1d743a7ae56bafff0b4775a54376af387077d9
yasicJW <joonoo3@inha.edu>
2022-08-08 20:03:36 +0900

    react ~#6

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./Button";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] =useState("");
+  const onClick = () => setValue((prev) => prev + 1 );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+    <input
+      value={keyword}
+      onChange={onChange}
+      type="text"
+      placeholder="Search here..."
+      />
+    <h1>{counter}</h1>
+    <Button onClick={onClick}></Button>
+   </div>
   );
 }
 
