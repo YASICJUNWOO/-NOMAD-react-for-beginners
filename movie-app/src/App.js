688f23f34899ba144ff5cb4631dfc693c1b01680
yasicJW <joonoo3@inha.edu>
2022-08-10 02:54:45 +0900

    movie-app #2

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async() => {
+    const response = await fetch(`https://api.themoviedb.org/3/trending/all/week?api_key=f2ac112a26588a2a83386a2b79a6bc25`);
+    const json = await response.json();
+    setMovies(json.results);
+    setLoading(false);
+  };
+  
+  useEffect(()=>{
+    getMovies()
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {
+      loading ? 
+      (<h1>Loading...</h1>) 
+      : (<div>
+          {movies.map((movie) => (<div key={movie.id}>
+            <h2 style={{color:"tomato",}}>{movie.title}</h2>
+            <p>{movie.overview}</p>
+            <img src={`http://image.tmdb.org/t/p/w500/${movie.poster_path}`}></img>
+            </div>))}
+          </div>
+        )
+      }
     </div>
   );
 }
